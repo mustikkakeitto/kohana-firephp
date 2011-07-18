c84597202c8b254ab163d48b16763e52807991af
Kemal Delalic <kemal.delalic@gmail.com>
2011-07-18 14:42:28 +0200

    FirePHP logging is disabled in production.

--- init.php
+++ init.php
@@ -1,5 +1,13 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
+// Find and include the vendor
 require_once Kohana::find_file('vendor/FirePHPCore','FirePHP.class');
 
+// Attach a Fire_Log writer to Kohana
 Kohana::$log->attach(new Fire_Log);
+
+// Disable FirePHP logging in production phase
+if (Kohana::$environment > Kohana::PRODUCTION)
+{
+	FirePHP::getInstance()->setEnabled(FALSE);
+}
