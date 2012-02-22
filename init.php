ada02acebbe7b97909966814eca544c1b31b4434
Kemal Delalic <kemal.delalic@gmail.com>
2012-02-22 23:48:15 +0100

    Changed the init file to check for the vendor class

--- init.php
+++ init.php
@@ -1,16 +1,27 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-// Find and include the vendor
-require_once Kohana::find_file('vendor/FirePHPCore','FirePHP.class');
+// Find the FirePHP vendor class
+$vendor_path = Kohana::find_file('vendor/FirePHPCore','FirePHP.class');
 
-$fire_logger = new Fire_Log(array(
-	
-));
+if ($vendor_path === FALSE)
+{
+	/**
+	 * If the vendor doesn't exist, user probably didn't update submodules so
+	 * warn him instead of ending up with a fatal error. Updating submodules:
+	 *
+	 * 		git submodule update --init
+	 */
+	throw new Kohana_Exception('You must update submodules (vendor class) to make FirePHP work with Kohana.');
+}
+
+require_once $vendor_path;
+
+$fire_logger = new Fire_Log;
 
 // Attach a Fire_Log writer to Kohana
 Kohana::$log->attach($fire_logger);
 
-// Disable FirePHP logging in production phase
+// Automatically disable FirePHP logging in production phase
 if (Kohana::$environment === Kohana::PRODUCTION)
 {
 	$fire_logger->writer()->setEnabled(FALSE);
